9001ace7d23e8386417c882c7b5ef62f69a639a0
David <forpersonalonly191@gmail.com>
2023-08-09 16:10:19 +0800

    Development

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 function Header() {
   return (
     <div className="navbar bg-transparent sticky top-0 z-50">
-    <div className="navbar-start z-50">
-     
+    <div className="navbar-start z-50">     
       <a className="btn btn-ghost normal-case text-xl z-50" href='/'>JOB ERA</a>
     </div>
-    <div className="navbar-center hidden lg:flex z-50">
+    <div className="navbar-center hidden lg:flex z-50 justify-between">
       <ul className="menu menu-horizontal px-1">
         <li><a href='/jobs'>Jobs</a></li>
         <li >
@@ -15,13 +14,10 @@ function Header() {
         </li>
         <li><a href='/apply'>Apply</a></li>
       </ul>
-    </div>
-    <div className="navbar-end z-50">
-    
       <a className="btn mx-2 bg-blue-700 text-white" href='/login'>Post Jobs</a>
       <a className="btn bg-green-500 text-white">Find Jobs</a>
     </div>
-  <div className='w-[200px]'></div>
+ 
     
   </div>
   )
