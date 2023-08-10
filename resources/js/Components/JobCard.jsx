fb48699fcdbf9e23fa365c55fffad31556ab5b57
Gossip <132025922+gossipme@users.noreply.github.com>
2023-08-10 16:22:41 +0800

    updated

--- resources/js/Components/JobCard.jsx
+++ resources/js/Components/JobCard.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Card,
     CardHeader,
     CardBody,
     Typography,
+    Popover,
+    PopoverHandler,
+    PopoverContent,
     Button,
 } from "@material-tailwind/react";
 
@@ -33,6 +36,7 @@ function JobCard(props) {
                     <Typography variant="h4" color="blue-gray" className="mb-2">
                         {props.company}
                     </Typography>
+                    <Typography className="text-xl">Job Description</Typography>
                     <Typography color="gray" className="mb-8 font-normal">
                         Like so many organizations these days, Autodesk is a
                         company in transition. It was until recently a
@@ -40,28 +44,10 @@ function JobCard(props) {
                         its own business model disruption is only part of the
                         story
                     </Typography>
-                    <a href="#" className="inline-block">
-                        <Button
-                            variant="text"
-                            className="flex items-center gap-2"
-                        >
-                            Learn More
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                className="w-4 h-4"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                                />
-                            </svg>
-                        </Button>
-                    </a>
+                    <div className="flex justify-between">
+                        <button>More Info</button>
+                        <h1>{props.vacancy} Vacancies</h1>
+                    </div>
                 </CardBody>
             </Card>
         </div>
---
+++ resources/js/Components/JobOverview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JobOverview() {
+    return (
+        <div>
+            <h1>JobOverview</h1>
+        </div>
+    );
+}
+
+export default JobOverview;
